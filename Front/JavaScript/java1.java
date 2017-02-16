8bab891ee4e0db56b8b267b5e9f5b7cde8f751f9
FelixROC <felix2000f@hotmail.com>
2017-02-16 16:19:36 +0100

    Add files via upload

---
+++ Front/JavaScript/java1.java
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>eerste javascript</title>
+	
+</head>
+
+<body>
+<h1> Alert box </h1>
+<script>
+ alert('hello world - vanuit JavaScript');
+</script>
+</body>
+</html>
